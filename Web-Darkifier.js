f94898c5822b414c620ae2480b32739a3b053604
Unknown <anthonystepka85@gmail.com>
2018-03-25 10:56:41 -0500

    Added more metadata blocks including an auto updater

--- Web-Darkifier.js
+++ Web-Darkifier.js
@@ -3,10 +3,13 @@
 // @namespace    web-darkifier
 // @version      0.3
 // @author       ATS Business and Games
+// @author       ATS Business and Games
 // @description  This userscript aims to make the web easier on your eyes by making websites have darker backgrounds.
 // @homepage     https://github.com/atsbusinessandgames/Web-Darkifier
+// @homepage     https://github.com/atsbusinessandgames/Web-Darkifier
 // @downloadURL  https://raw.githubusercontent.com/atsbusinessandgames/Web-Darkifier/master/Web-Darkifier
-
+// @downloadURL  https://raw.githubusercontent.com/atsbusinessandgames/Web-Darkifier/master/Web-Darkifier.js
+// @supportURL   https://github.com/atsbusinessandgames/Web-Darkifier/issues
 //@include /(http|https):\/\/www\.(cnet|instagram|ketv|snopes|thewindowsclub|zdnet)\.com\/.*/
 
 // @grant        GM_addStyle
